bccb637530ed0f1636edf97bb7d9423463381299
Raphael Marques <raphaeljcmarques@gmail.com>
2022-04-19 21:05:57 -0300

    Raphael's part done

--- script.js
+++ script.js
@@ -22,4 +22,44 @@ function menuToggle() {
   });
 }
 
-menuToggle();
+function navigation() {
+  const img = document.querySelector('#current-user-id');
+  const arrowLeft = document.querySelector('#arrow-left');
+  const arrowRight = document.querySelector('#arrow-right');
+  const imgAdd = document.querySelector('#add');
+  let counter = 1;
+
+
+  arrowLeft.addEventListener('click', () => {
+    counter === 1 ? counter = 1 : counter--;
+    img.src = `../assets/img${counter}.png`;
+  });
+
+  arrowRight.addEventListener('click', () => {
+    counter === 5 ? counter = 5 : counter++;
+    img.src = `../assets/img${counter}.png`;
+  });
+
+  img.addEventListener('mouseover', () => {
+    imgAdd.classList.add('active');
+  });
+
+  img.addEventListener('mouseout', () => {
+    imgAdd.classList.remove('active');
+  });
+
+  imgAdd.addEventListener('mouseover', () => {
+    imgAdd.classList.add('active');
+  });
+
+  imgAdd.addEventListener('mouseout', () => {
+    imgAdd.classList.remove('active');
+  });
+}
+
+function init() {
+  menuToggle();
+  navigation();
+}
+
+init();
